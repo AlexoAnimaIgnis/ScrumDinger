52346abe57620cc6d8c6103c9db8db4f01c0e004
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2022-04-04 06:53:16 +0800

    SwiftUI_Sec1: Using Stacks

---
+++ Scrumdringer/ScrumdringerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdringerApp.swift
+//  Scrumdringer
+//
+//  Created by Alexo Ballester on 4/2/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdringerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MeetingView()
+        }
+    }
+}
